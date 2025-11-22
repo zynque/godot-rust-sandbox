323e22d5972517c756e30c2825adf37e814685bb
zynque <zynque@github.com>
2025-11-22 15:43:41 -0800

    mouse input handling in rust

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ The following need to be installed in order for this to run:
  * Rust
 * Visual Studio Community Edition (For C++ linker)
 
+As for the editor, I prefer Visual Studio Code with the rust-analyzer plugin
+
 __Note__ that the Godot version and the godot-rust version need to be compatible. Godot-Rust docs were out of date in this regard. Look at release notes for godot-rust to see exactly what is supported when. In this project the godot-rust version is currently set to "0.3.5" and Godot is 4.4.
 
 Once the required tools are installed, run `cargo build` from the rust folder, and then import the project in Godot and run it.
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,6 @@
 use godot::prelude::*;
+use godot::classes::{ISprite2D, InputEventMouseButton, Sprite2D};
+use godot::classes::{InputEvent};
 use crate::point_vec_extensions::PointVecExtensions;
 
 pub mod point_vec_extensions;
@@ -12,6 +14,7 @@ unsafe impl ExtensionLibrary for GodotRustExtension {}
 #[class(base=Node2D)]
 struct RustCanvas {
     #[base]
+    angular_speed: f64,
     base: Base<Node2D>
 }
 
@@ -20,7 +23,10 @@ impl INode2D for RustCanvas {
     fn init(base: Base<Node2D>) -> Self {
         godot_print!("RustCanvas initialized");
 
-        Self { base }
+        Self {
+            angular_speed: std::f64::consts::PI,
+            base,
+        }
     }
 
     fn draw(&mut self) {
@@ -30,12 +36,60 @@ impl INode2D for RustCanvas {
             Vector2::new(-50.0, 50.0),
         ];
 
-        let translated_points = points.translated(Vector2::new(100.0, 100.0));
+        let translated_points = points.translated(Vector2::new(10.0, 10.0));
         let packed_points = PackedVector2Array::from(translated_points);
 
         let color = Color::from_rgb(0.2, 0.8, 0.3);
 
         // Draw filled polygon
         self.base_mut().draw_colored_polygon(&packed_points, color);
+    }
+
+    fn physics_process(&mut self, delta: f64) {
+        // In GDScript, this would be: 
+        // rotation += angular_speed * delta
+        
+        let radians = (self.angular_speed * delta) as f32;
+        self.base_mut().rotate(radians);
+        // The 'rotate' method requires a f32, 
+        // therefore we convert 'self.angular_speed * delta' which is a f64 to a f32
+    }
+
+    fn input(&mut self, event: Gd<InputEvent>) {
+        if let Ok(mouse_event) = event.try_cast::<InputEventMouseButton>() {
+            if mouse_event.is_pressed() {
+                let p = mouse_event.get_position();
+                self.base_mut().set_position(p);
+            }
+        }
+    }
+}
+
+#[derive(GodotClass)]
+#[class(base=Sprite2D)]
+struct Player {
+    angular_speed: f64,
+    base: Base<Sprite2D>
+}
+
+#[godot_api]
+impl ISprite2D for Player {
+    fn init(base: Base<Sprite2D>) -> Self {
+        godot_print!("Hello, world!"); // Prints to the Godot console
+        
+        Self {
+            angular_speed: std::f64::consts::PI,
+            base,
+        }
+    }
+
+    fn physics_process(&mut self, delta: f64) {
+        // In GDScript, this would be: 
+        // rotation += angular_speed * delta
+        
+        let radians = (self.angular_speed * delta) as f32;
+        self.base_mut().rotate(radians);
+        // The 'rotate' method requires a f32, 
+        // therefore we convert 'self.angular_speed * delta' which is a f64 to a f32
     }
 }
