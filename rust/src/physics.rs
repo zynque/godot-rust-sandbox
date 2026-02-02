9f2317ac7546a3f568b0cabbeb4e12addf85b679
zynque <zynque@github.com>
2026-02-02 15:23:29 -0800

    wrapper for body test motion

--- rust/src/physics.rs
+++ rust/src/physics.rs
@@ -1,3 +1,4 @@
+use godot::classes::{PhysicsTestMotionParameters2D, PhysicsTestMotionResult2D};
 use godot::prelude::*;
 use godot::builtin::{
   Rid
@@ -6,9 +7,8 @@ use godot::classes::{
     PhysicsServer2D,
     PhysicsDirectSpaceState2D,
     PhysicsShapeQueryParameters2D,
-    Shape2D,
-    Engine,
-    SceneTree,
+    physics_server_2d::BodyMode,
+    physics_server_2d::BodyState
 };
 
 pub struct GodotPhysics{
@@ -21,6 +21,8 @@ pub trait GodotPhysicsSpace {
   fn add_area_polygon(&self, polygon: &Vec<Vector2>, position: Vector2) -> Rid;
   fn polygon_collides(&mut self, polygon: &Vec<Vector2>, position: Vector2) -> bool;
   fn cast_motion(&mut self, polygon: &Vec<Vector2>, current_pos: Vector2, movement: Vector2) -> MoveResult;
+  fn create_body(&mut self, polygon: &Vec<Vector2>, position: Vector2) -> Rid;
+  fn body_test_motion(&mut self, body_rid: Rid, movement: Vector2) -> BodyTestResult;
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -28,6 +30,17 @@ pub struct MoveResult {
   pub motion: Vector2,
   pub remainder: Vector2,
   pub collided: bool,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct BodyTestResult {
+  pub collided: bool,
+  pub travel: Vector2,
+  pub remainder: Vector2,
+  pub safe_fraction: f32,
+  pub collision_point: Vector2,
+  pub collision_normal: Vector2,
+  pub collider_rid: Rid,
 }
 
 // fn world_space_physics() -> Result<GodotPhysics> {
@@ -111,6 +124,48 @@ impl GodotPhysicsSpace for GodotPhysics {
       motion,
       remainder,
       collided,
+    }
+  }
+
+  fn create_body(&mut self, polygon: &Vec<Vector2>, position: Vector2) -> Rid {
+    let mut server = PhysicsServer2D::singleton();
+    let body_rid = server.body_create();
+    server.body_set_mode(body_rid, BodyMode::KINEMATIC);
+    server.body_set_space(body_rid, self.space);
+    
+    let shape_rid = server.convex_polygon_shape_create();
+    server.shape_set_data(shape_rid, &Variant::from(PackedVector2Array::from(polygon.as_slice())));
+    
+    server.body_add_shape(body_rid, shape_rid);
+
+    let mut t = server.body_get_state(body_rid, BodyState::TRANSFORM).to::<Transform2D>();
+    t.origin = position;
+    server.body_set_state(body_rid, BodyState::TRANSFORM, &Variant::from(t));
+    
+    body_rid
+  }
+
+  fn body_test_motion(&mut self, body_rid: Rid, movement: Vector2) -> BodyTestResult {
+    let mut server = PhysicsServer2D::singleton();
+
+    let current_transform = server.body_get_state(body_rid, BodyState::TRANSFORM).to::<Transform2D>();
+
+    let mut parameters = PhysicsTestMotionParameters2D::new_gd();
+    parameters.set_from(current_transform);
+    parameters.set_motion(movement);
+
+    let result = PhysicsTestMotionResult2D::new_gd();
+
+    let collided = server.body_test_motion_ex(body_rid, &parameters).result(&result).done();
+
+    BodyTestResult {
+      collided: collided,
+      travel: result.get_travel(),
+      remainder: result.get_remainder(),
+      safe_fraction: result.get_collision_safe_fraction(),
+      collision_point: result.get_collision_point(),
+      collision_normal: result.get_collision_normal(),
+      collider_rid: result.get_collider_rid(),
     }
   }
 }
